80ffd6835c05411aacb4074d96fda173f9df7074
Chelley <michelle.owodunni@gmail.com>
2019-12-17 23:28:41 -0600

    ee addition

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
     console.log('\n')
-
+    firstPrompt()
 });
 
 function firstPrompt() {
@@ -29,24 +29,37 @@ function firstPrompt() {
         choices: ['View All Employees', 'View All Departments', 'View All Roles', 'Add Employee', 'Add Department', 'Add Role', 'Update Role'],
         name: "initialOptions"
     })
+
+
+    .then(function(response) {
+        if (response.initialOptions === 'View All Employees') {
+            queryAllEmployees();
+        } else {
+            console.log("bye");
+        }
+    })
 }
 
-//     .then(function(response) {
-//         if (response.initialOptions === 'View All Employees') {
-//             postItem();
-//         } else {
-//             bidItem()
-//         }
-//     })
-// }
+function queryAllEmployees() {
+    connection.query("SELECT * FROM employee", function(err, res) {
+        if (err) throw err;
+        for (let i = 0; i < res.length; i++) {
+            console.log(res[i].id + " | " + res[i].first_name + " | " + res[i].last_name + " | " + res[i].role_id + " | " + res[i].manager_id);
+        }
+        console.log("-----------------------------------");
+    });
+}
 
-// function queryAllDepartments() {
-//     connection.query("SELECT * FROM departments", function(err, res) {
-//         if (err) throw err;
-//         for (let i = 0; i < res.length; i++) {
-//             console.log(res[i].id + " | " + res[i].name);
+// example for adding data
+// function addRow() {
+//     connection.query(`INSERT INTO items(name, price, category) VALUES("${name}", ${price}, "${category}");`, function (err, res) {
+//         if (err) {
+//             console.log(err)
 //         }
+//         console.log('Item added!! Bringing you back to home page.');
 //         console.log("-----------------------------------");
+
+//         firstPrompt();
 //     });
 // }
 
@@ -54,6 +67,10 @@ function firstPrompt() {
 
 // }
 
-// function Employee() {
+// function addEmployee() {
+
+// }
+
+// function updateRole() {
 
 // }
